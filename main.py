5af9080f0184b26333594435893a503841c88222
Hithy <55604744+xHithy@users.noreply.github.com>
2025-05-20 21:05:56 +0300

    Gathered together program logic

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from linkedList import LinkedList
+from excel import update_excel, create_email_linked_list, mark_emails_as_sent
+from seleniumFunctions import send_email
+import os
 
 
-# Press the green button in the gutter to run the script.
+def read_emails(template_name):
+    file_path = f"address_lists/{template_name}"
+    if not os.path.exists(file_path):
+        print(f"No such file or directory: {file_path}")
+        return LinkedList()
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    email_linked_list = LinkedList()
+    for line in lines:
+        email_linked_list.append(line.strip())
+
+    return email_linked_list
+
+def print_emails(template_name):
+    file_path = f"address_lists/{template_name}"
+    if not os.path.exists(file_path):
+        print(f"No such file or directory: {file_path}")
+        return LinkedList()
+    print(f'Emails in address_lists/{template_name} :')
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    for line in lines:
+        print(line.strip())
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        command = input("Please enter command (print_file / update_excel / send_email / exit): ").strip()
+        if command == "print_file":
+            file_name = input("Enter the name of the file:").strip()
+            print_emails(file_name)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        elif command == "update_excel":
+            file_name = input("Enter the name of the file to update Excel from:").strip()
+            email_list = read_emails(file_name)
+            update_excel(email_list)
+            print("New emails added!")
+
+        elif command == "send_email":
+            file_name = input("Enter path to filename to take content. ").strip()
+            if not os.path.exists(f"content/{file_name}"):
+                print(f"File not found: content/{file_name}")
+                continue
+            email_listFromExel = create_email_linked_list('mails.xlsx')
+            if email_listFromExel.length > 0:
+                if send_email(email_listFromExel, file_name):
+                    mark_emails_as_sent(email_listFromExel)
+            else:
+                print("Emails already are sent to all addresses!")
+
+        elif command == "exit":
+            print("Exiting program.")
+            break
+
+        else:
+            print("Unknown command. Try again.")
+
+
