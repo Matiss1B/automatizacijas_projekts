18bdbaa6f562ea131fcc8c7b151ec46e3cc63a3c
Hithy <55604744+xHithy@users.noreply.github.com>
2025-05-20 21:05:34 +0300

    Added custom data structure

---
+++ linkedList.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+        if value is not None:
+            self.append(value)
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def __iter__(self):
+        self.current = self.head
+        return self
+
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        value = self.current.value
+        self.current = self.current.next
+        return value
